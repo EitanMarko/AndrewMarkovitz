9c3ce53a769995c6f2904c1e354d32cbdf14208f
Eitan <amarkov3@mail.yu.edu>
2025-06-04 13:32:49 -0400

    Adding new project ProductiviMe

---
+++ ProductiviMe/src/main/java/com/eitan/productivime/ProductiviMeApplication.java
@@ -0,0 +1,13 @@
+package com.eitan.productivime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductiviMeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductiviMeApplication.class, args);
+    }
+
+}
