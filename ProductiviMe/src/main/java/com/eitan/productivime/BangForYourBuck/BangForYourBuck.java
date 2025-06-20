95e45f3529fae8744412cf92dcaa1b6e76255429
Eitan <amarkov3@mail.yu.edu>
2025-06-20 13:53:08 -0400

    Plane and Proctoring Work

--- ProductiviMe/src/main/java/com/eitan/productivime/BangForYourBuck/BangForYourBuck.java
+++ ProductiviMe/src/main/java/com/eitan/productivime/BangForYourBuck/BangForYourBuck.java
@@ -20,6 +20,7 @@ public class BangForYourBuck {
     private List<Activity> flexActivities;
     private List<Activity> doTodayActivities;
     private Set<String> activityNames;
+    private List<Interval> filledIntervals;
 
     private int dayStart;
     private int dayEnd;
@@ -42,6 +43,7 @@ public class BangForYourBuck {
         flexActivities = new ArrayList<>();
         doTodayActivities = new ArrayList<>();
         activityNames = new HashSet<>();
+        filledIntervals = new ArrayList<>();
 
 
         //Now that activities have been parsed, we can choose the optimal day
@@ -49,7 +51,32 @@ public class BangForYourBuck {
 
     }
 
-    public void addActivity(Activity activity){
+    private class Schedule {
+
+        private int value;
+        private Double activitiesDone;
+        private int lastActivityIndex; //For backtracking - indicates the last activity done. Index is in the list of activities (whether actually or virtually contiguous)
+
+        public Schedule(int value, Double activitiesDone, int lastActivityIndex) {
+            this.value = value;
+            this.activitiesDone = activitiesDone;
+            this.lastActivityIndex = lastActivityIndex;
+        }
+
+        public Double getActivitiesDone() {
+            return activitiesDone;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        public int getLastActivityIndex() {
+            return lastActivityIndex;
+        }
+    }
+
+    public void addActivity(Activity activity, boolean addMultipleActivities){ // method can be called from user or addMultipleActivities()
 
         String name = activity.getName();
         if(activityNames.contains(name)){
@@ -57,11 +84,14 @@ public class BangForYourBuck {
         }
 
         if(activity instanceof SetActivity){
-            // Invalid interval - starts before day begins || finishes after day ends
-            if( ((SetActivity) activity).interval.start < dayStart || ((SetActivity) activity).interval.end > dayEnd){
-                return;
+            if (!addMultipleActivities) { // If addActivity() called by addMultipleActivities(), below checks already done
+                
+                // Invalid interval - starts before day begins || finishes after day ends
+                validIntervalCheck((SetActivity) activity); // Ensure activity's interval is within defined "day"
+                overlapCheck((SetActivity) activity); // Ensure new SetActivity doesn't overlap previously added SetActivities
             }
             setActivities.add(activity); // Valid interval - add to list
+            filledIntervals.add(((SetActivity) activity).interval);
         }
         else{ // FlexibleActivity or DoTodayFlexActivity
 
@@ -102,28 +132,148 @@ public class BangForYourBuck {
         activityNames.add(activity.getName()); // Track activity names so no repeats
     }
 
+    private void overlapCheck(SetActivity activity) {
+        for(Interval interval : filledIntervals){
+
+            //If the start time is between the start and end time of the interval
+            if(activity.interval.start >= interval.start && activity.interval.start < interval.end){
+                throw new IllegalArgumentException("Attempted to add setActivity which overlaps with previously added setActivity");
+            }
+            //If the end   time is between the start and end time of the interval
+            if(activity.interval.end > interval.start && activity.interval.end <= interval.end){
+                throw new IllegalArgumentException("Attempted to add setActivity which overlaps with previously added setActivity");
+            }
+        }
+    }
+
     private boolean isFlexibleActivity(Activity activity){
         if(activity instanceof FlexibleActivity){
             return true;
         }
         return false;
     }
+    
 
     public void addMultipleActivities(Activity... activities){
 
         // Check if there are any repeat names among the activities being added
         Set<String> repeats = new HashSet<>();
         for(int i = 0; i<activities.length; i++){
-            String activityName = activities[i].getName();
+            Activity activity = activities[i];
+            String activityName = activity.getName();
             if(activityNames.contains(activityName) || repeats.contains(activityName)){
                 throw new IllegalArgumentException("Repeat activity name: "+activityName);
             }
-            repeats.add(activityName);
+            repeats.add(activityName); // Track new activities in case of repeat names
+
+            if(activity instanceof SetActivity){
+                validIntervalCheck((SetActivity) activity); // Ensure activity's interval is within defined "day"
+                overlapCheck((SetActivity) activity); // Ensure new SetActivity doesn't overlap previously added SetActivities
+            }
+
+
         }
 
         for(Activity activity : activities) {
-            addActivity(activity);
+            addActivity(activity, true);
         }
+    }
+
+    private void validIntervalCheck(SetActivity activity) {
+        // Invalid interval - starts before day begins || finishes after day ends
+        if( activity.interval.start < dayStart || activity.interval.end > dayEnd){
+            throw new IllegalArgumentException("SetActivity must be set within daytime hours");
+        }
+    }
+
+    public void generateSchedule(){
+
+        Schedule schedule = new Schedule(0,0.0, -1);
+        int numOfActivities = setActivities.size() + doTodayActivities.size() + flexActivities.size();
+        int intervalStartTimes = (dayEnd - dayStart) / 5; //Activities can only occur at times of factor 5 (e.g. 1:00, 1:05, 1:10, etc.)
+        Schedule[][] dp = new Schedule[intervalStartTimes][numOfActivities];
+
+
+        //Create a two dimensional array -> [time][activity]
+
+        //Let's say that an activity has to be done at a time which is a multiple of 5
+            // Create a Time instance, divide the value by 5
+                // Ex: instead of 0:30 being 30, it's 6 because:
+                    // 0, 5, 10, 15, 20, 25, 30 (6th term in the sequence)
+            // Index:  0, 1,  2,  3,  4,  5    INDEX 6
+        // PROGRAM CAN BE MADE MORE EFFICIENT WITH LONGER INTERVALS (e.g. 15 mins), BUT THEN USER LOSES FLEXIBILTY
+            // More efficient because much smaller array
+
+        //In the array we store an object which holds two things:
+            // Current value
+            // Double to indicate completed activities ("activitiesDone")
+
+        //There will be a List to store these activities in set indices - SEE "ACTIVITIES LIST" BELOW
+
+        //To generate the next activity in your day (among possibilities):
+            // Check - Is there is a setActivity slated for that time (e.g. we're at 12:00 and setActivity "lunch" set for 12:00)?
+                // IF YES - Do that setActivity
+                //If NO:
+                    // take the activitiesDone (Double) and bitwise '&' with position of activity (for all activities)
+                        // If bit is not set, activity has not yet been done, and you can move forward in adding this to a schedule
+                // Why: Because if we don't do a setActivity, the Schedule we're creating will be thrown out anyway (non-viable Schedule)
+                    // Elminiates unnecessary work (efficient)
+                //I THINK THIS DOUBLE ENABLES AT MOST 64 ACTIVITIES TO BE INPUT. IF THIS IS THE CASE,
+                        // MAKE A CHECK IN addActivity() which caps the number of activities being added
+
+        // ACTIVITIES LIST - 2 options:
+            // 1) Don't make another list of activities, just keep the 3 lists, and consider them as one
+                // Ex: if there are 2 setActivities but we look for index 2 among ALL activities,
+                //      that will end up in index 0 of the doTodayActivities list
+                // ORDER OF VIRTUAL CONTIGUOUS LIST: setActivities, doTodayActivities, flexActivities
+            // 2) Make another list of activities, and when an activity is added to the specific list, add it also to the general list
+                // Ex: Adding a setActivity "eat"
+                    // Add "eat" to setActivities list
+                    // Add "eat" to allActivities list
+
+
+        //Generate possibilities by BFS
+
+        //If a given possibility is impossible because it would take you beyond the end of the day, don't generate it
+
+        //If a Schedule can't generate any further activities
+        // (e.g. day ends at 6:00, it's 5:45, and no activities take <=15 mins),
+        //Make a check on that Schedule
+
+        //What the check includes:
+            // 1) Is the value of the Node greater than "int greatestValue"?
+                // greatestValue is instantiated at 0
+                // If Schedule.value > greatestValue, it's a candidate
+                // If Schedule.value == greatestValue, we will do a TIEBREAKER (COME BACK TO THIS) - What makes a schedule more valuable than another?
+            // 2) Are all setActivities completed in this schedule?
+            // 3) Are all doTodayFlexActivities completed in this schedule?
+                // To check #2,3, bitwise '&' Node.activitiesDone with positions of all setActivities and doTodayFlexActivities
+        //IF THESE CONDITIONS PASS, SAVE THE NODE AS THE CURRENT BEST SCHEDULE
+        //WHEN BFS ENDS, WHATEVER IS SAVED IS THE FINAL SCHEDULE
+
+
+        // FOR LATER - Create a checks in addActivity() which don't allow:
+        //      a setActivity to be added if it overlaps with another setActivity
+        //      a doTodayFlexActivity to be added if there is no available space for this activity to occur based on
+        //          previously input setActivities and doTodayFlexActivities
+            // Reasoning: because then no schedule will be viable
+
+        // Ex: Day is 10:00-12:00
+            // Adding setActivity 10:00-11:00 - ACCEPTED
+            // Adding setActivity 10:30-10:45 - REJECTED
+            // Adding setActivity 11:30-12:00 - ACCEPTED
+            // Adding doTodayFlexActivity 15 mins - ACCEPTED
+            // Adding doTodayFlexActivity 45 mins - REJECTED
+        //THIS MAY BE VERY HARD TO DO BECAUSE OF CERTAIN SCENARIOS (e.g. Adding a setActivity after many doTodayFlexActivities have been added)
+            // SO MAYBE JUST DO THIS FOR setActivities
+
+        //TO GET ACTUAL SCHEDULE - BACKTRACKING
+            // A 'Schedule' object holds an 'int lastActivityIndex' which indicates the index
+            //   in the list of activities (virtually or actually contiguous) of the previous activity
+            // 'int lastActivityTime' = find the duration of the last activity, subtract that from the current time
+            // To backtrack traverse back to dp[lastActivityTime][lastActivityIndex]
+                //If lastActivityIndex == -1, this indicates first activity in schedule (end of backtracking)
+
     }
 
     public List<Activity> getFlexActivities() {
--- ProductiviMe/src/test/java/com/eitan/productivime/BangForYourBuckTest.java
+++ ProductiviMe/src/test/java/com/eitan/productivime/BangForYourBuckTest.java
@@ -14,6 +14,8 @@ public class BangForYourBuckTest {
 
         // INVALID INTERVALS
 
+            // Add Activities individually
+
         Interval interval1 = new Interval("8:45","17:15"); // Both start and end out of bounds
         Activity act1 = new SetActivity("act1",interval1);
 
@@ -24,33 +26,134 @@ public class BangForYourBuckTest {
         Activity act3 = new SetActivity("act3",interval3);
 
         BangForYourBuck bangForYourBuckEmpty = new BangForYourBuck("9:00","17:00");
-        bangForYourBuckEmpty.addActivity(act1);
-        bangForYourBuckEmpty.addActivity(act2);
-        bangForYourBuckEmpty.addActivity(act3);
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            bangForYourBuckEmpty.addActivity(act1, false);});
+        assertThrows(IllegalArgumentException.class, () -> {
+            bangForYourBuckEmpty.addActivity(act2, false);});
+        assertThrows(IllegalArgumentException.class, () -> {
+            bangForYourBuckEmpty.addActivity(act3, false);});
 
         List<Activity> emptyList= new ArrayList<>();
         assertEquals(emptyList, bangForYourBuckEmpty.getSetActivities()); //No activities were added
 
-        bangForYourBuckEmpty.addMultipleActivities(act1, act2, act3);
+
+            // Add Activities all at once
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            bangForYourBuckEmpty.addMultipleActivities(act1, act2, act3);});
         assertEquals(emptyList, bangForYourBuckEmpty.getSetActivities()); //No activities were added
 
 
         // VALID INTERVALS
 
-        Interval interval4 = new Interval("9:00","17:00"); // Only start out of bounds
+            // Add Activities individually
+
+        Interval interval4 = new Interval("9:00","11:00"); // Interval in bounds
         Activity act4 = new SetActivity("act4",interval4);
 
-        Interval interval5 = new Interval("11:30","14:15"); // Only start out of bounds
+        Interval interval5 = new Interval("11:30","14:15"); // Interval in bounds
         Activity act5 = new SetActivity("act5",interval5);
 
         BangForYourBuck bangForYourBuckFull = new BangForYourBuck("9:00","17:00");
-        bangForYourBuckFull.addActivity(act4);
-        bangForYourBuckFull.addActivity(act5);
+        bangForYourBuckFull.addActivity(act4, false);
+        bangForYourBuckFull.addActivity(act5, false);
 
         List<Activity> nonEmptyList= new ArrayList<>();
         nonEmptyList.add(act4);
         nonEmptyList.add(act5);
         assertEquals(nonEmptyList,bangForYourBuckFull.getSetActivities());
+
+
+        Interval interval6 = new Interval("9:45","10:00");
+        Activity act6 = new SetActivity("act6",interval6);
+
+        Interval interval7 = new Interval("10:15","10:30");
+        Activity act7 = new SetActivity("act7",interval7);
+
+        Interval interval8 = new Interval("10:45","11:00");
+        Activity act8 = new SetActivity("act8",interval8);
+
+
+            // Add Activities all at once
+
+        bangForYourBuckFull.addMultipleActivities(act6, act7, act8);
+
+        nonEmptyList.add(act6);
+        nonEmptyList.add(act7);
+        nonEmptyList.add(act8);
+
+        assertEquals(nonEmptyList,bangForYourBuckFull.getSetActivities());
+    }
+
+    @Test
+    void overlappingIntervals() { // SetActivities may not overlap
+
+        List<Activity> activityList= new ArrayList<>(); // List of viable activities
+
+        Interval interval1 = new Interval("10:00","12:00"); // Valid interval
+        Activity act1 = new SetActivity("act1",interval1);
+
+        Interval interval2 = new Interval("11:00","11:30");  // Interval falls within (overlaps) interval1
+        Activity act2 = new SetActivity("act2",interval2);
+
+        BangForYourBuck bangForYourBuck = new BangForYourBuck("6:00","22:00");
+        bangForYourBuck.addActivity(act1, false);
+        activityList.add(act1);
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            bangForYourBuck.addActivity(act2, false);});
+        assertThrows(IllegalArgumentException.class, () -> {
+            bangForYourBuck.addMultipleActivities(act2);});
+
+        // Keep track of setActivity intervals, and for a given add, check whether there is overlap
+
+        Interval interval3 = new Interval("9:00","11:00"); // Interval overlaps interval1
+        Activity act3 = new SetActivity("act3",interval3);
+
+        Interval interval4 = new Interval("11:00","13:00"); // Interval overlaps interval1
+        Activity act4 = new SetActivity("act4",interval4);
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            bangForYourBuck.addActivity(act3, false);});
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            bangForYourBuck.addActivity(act4, false);});
+
+
+        //Activities may be assigned back-to-back (e.g. 10:00-12:00 exists, can also add 9:00-10:00 and 12:00-13:00)
+
+        // Add Activities individually
+
+        Interval interval5 = new Interval("9:00","10:00");
+        Activity act5 = new SetActivity("act5",interval5);
+
+        Interval interval6 = new Interval("12:00","13:00");
+        Activity act6 = new SetActivity("act6",interval6);
+
+        bangForYourBuck.addActivity(act5, false);
+        bangForYourBuck.addActivity(act6, false);
+        activityList.add(act5);
+        activityList.add(act6);
+
+        // Now 9:00-13:00 is reserved for setActivities
+        assertEquals(activityList,bangForYourBuck.getSetActivities());
+
+
+        // Add Activities all at once
+
+        Interval interval7 = new Interval("8:00","9:00");
+        Activity act7 = new SetActivity("act7",interval7);
+
+        Interval interval8 = new Interval("13:00","14:00");
+        Activity act8 = new SetActivity("act8",interval8);
+
+        bangForYourBuck.addMultipleActivities(act7,act8);
+        activityList.add(act7);
+        activityList.add(act8);
+
+        // Now 8:00-14:00 is reserved for setActivities
+        assertEquals(activityList,bangForYourBuck.getSetActivities());
     }
 
     @Test
@@ -173,9 +276,9 @@ public class BangForYourBuckTest {
 
         BangForYourBuck bangForYourBuck2 = new BangForYourBuck("9:00","20:00");
 
-        bangForYourBuck2.addActivity(act2);
+        bangForYourBuck2.addActivity(act2, false);
         assertThrows(IllegalArgumentException.class, () -> {
-            bangForYourBuck2.addActivity(act1);});
+            bangForYourBuck2.addActivity(act1, false);});
 
 
         //Repeat of activity already added
@@ -184,7 +287,7 @@ public class BangForYourBuckTest {
         Activity act4 = new FlexibleActivity("thing","13:00","17:00","2:00", 10);
 
         BangForYourBuck bangForYourBuck3 = new BangForYourBuck("9:00","20:00");
-        bangForYourBuck3.addActivity(act3);
+        bangForYourBuck3.addActivity(act3, false);
 
 
         assertThrows(IllegalArgumentException.class, () -> {
